8126ee145398291bd3effe1a506ee89c1ce7bcaf
joyr <swd@joyr.net>
2024-01-19 12:28:41 +0000

    エラー発生時にメールを送信する（重複送信防止）

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,15 @@
 
 namespace App\Exceptions;
 
+use App\Exceptions\MailReport;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    private MailReport $mailReport;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -18,6 +22,12 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function __construct(Container $container, MailReport $mailReport)
+    {
+        $this->mailReport = $mailReport;
+        parent::__construct($container);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      */
@@ -27,4 +37,14 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function report(Throwable $exception)
+    {
+        if ($this->shouldReport($exception)) {
+            $this->mailReport->send($exception);
+        }
+
+        parent::report($exception);
+    }
+
 }
---
+++ resources/views/mail/report/exception.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error Report</title>
+</head>
+<body>
+    <h1>Error Report</h1>
+    <dl>
+        <dt>Error Information<dt>
+        <dd>@php dump($error) @endphp</dd>
+        <dt>Server Information<dt>
+        <dd>@php dump($server) @endphp</dd>
+    </dl>
+</body>
+</html>
