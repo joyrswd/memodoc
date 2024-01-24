beea27fa733b5c62dc41f6c8429a864f321c7a10
joyrswd <5625893+joyrswd@users.noreply.github.com>
2024-01-24 04:20:17 +0000

    タグ登録方法変更

--- resources/views/memo/edit.blade.php
+++ resources/views/memo/edit.blade.php
@@ -9,13 +9,24 @@
         @method('PUT')
         <div class="mt-3">
             <p class="p-2" data-x="post">{!! nl2br($memo['content']) !!}</p>
-        </div>
-        <div class="mt-3">
-            <label for="tags">タグ</label>
-            <input type="text" data-x="tag" class="form-control" name="memo_tags" id="tags" minlength="2" maxlength="255" placeholder="空白区切りで複数登録（記号不可）" value="{{old('memo_tags', implode(' ', $memo['tags']))}}">
+            <div data-x="tags">
+                <div class="row">
+                    <div class="col-10">
+                        @foreach($memo['tags'] as $tag)
+                        <input type="hidden" name="tags[]" value="{{$tag}}">
+                        @endforeach
+                    </div>
+                </div>
+            </div>
             @error('tags.*')
-            <span class="invalid-feedback">{{ $message }}</span>
+            <p><span class="invalid-feedback">{{ $message }}</span></p>
             @enderror
+            <div class="mt-2 text-end" data-x="controller">
+                <small class="form-switch me-2">
+                    <label class="form-check-label align-baseline" for="flexSwitchCheckDefault">タグ登録</label>
+                    <input class="form-check-input m-0 align-text-bottom" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="has_tag" checked>
+                </small>
+            </div>
         </div>
         <div class="mt-5 col-lg-6 mx-auto">
             <span class="d-grid gap-3"><button type="submit" class="btn btn-primary">タグ再設定</button></span>
